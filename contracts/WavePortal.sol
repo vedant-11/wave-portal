15bb95349e47c7f59dc27b2a6239b57d96ce622d
vedant-11 <69393820+vedant-11@users.noreply.github.com>
2022-02-05 22:40:35 +0530

    initiated smart contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("hi this is tom");
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,20 @@
+const { hexStripZeros } = require("ethers/lib/utils");
+
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("wave contract deployed to:", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+runMain();
